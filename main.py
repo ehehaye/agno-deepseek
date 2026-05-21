6f772de0e505e0c9bdd02273fa7db0ef2e2543e4
ehehaye <hongxin.tang@hotmail.com>
2026-05-21 11:05:23 +0800

    ✨ Add team and multi-agent examples

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from agno.agent import Agent
+from agno.team import Team
 from agno.models.deepseek import DeepSeek
 from agno.os import AgentOS
 from agno.db.sqlite import SqliteDb
 
-agent = Agent(
+basicAgent = Agent(
+    name="basic agent",
     model=DeepSeek(id="deepseek-chat"),
+    markdown=True,
+)
+
+reasonAgent = Agent(
+    name="reason agent",
+    model=DeepSeek(id="deepseek-v4-flash"),
     reasoning=True,          # enable reasoning capabilities
     reasoning_max_steps=5,   # optional: set maximum reasoning steps
+    markdown=True,
+)
+
+team = Team(
+    name="My Team",
+    description="A team of agents working together",
+    model=DeepSeek(id="deepseek-chat"),
+    members=[reasonAgent, basicAgent],
     markdown=True,
 )
 
@@ -14,8 +30,8 @@ db = SqliteDb(db_file="data/agentos.db")
 
 agent_os = AgentOS(
     name="My AgentOS",
-    agents=[agent],
-    teams=None,
+    agents=[reasonAgent, basicAgent],
+    teams=[team],
     workflows=None,
     knowledge=None,
     db=db,
